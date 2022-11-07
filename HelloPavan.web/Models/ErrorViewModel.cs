3e370389b9c3ab3d8628639d2a89a9a51d25a683
pavan <pavanshivanand@gmail.com>
2022-11-07 16:29:05 +0530

    first

---
+++ HelloPavan.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloPavan.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
